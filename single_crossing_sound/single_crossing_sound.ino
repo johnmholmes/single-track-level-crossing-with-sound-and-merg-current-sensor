88f9b503f1addaae33ed49b3fa7537f80aea44ca
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2022-04-24 20:25:31 +0100

    Update single_crossing_sound.ino
    The code is being re commented to make it easier to understand.

--- single_crossing_sound/single_crossing_sound.ino
+++ single_crossing_sound/single_crossing_sound.ino
@@ -6,24 +6,44 @@
  * there are 6 free io pins left for other uses
  * the code is adaptation of https://rudysarduinoprojects.wordpress.com/2020/09/23/fun-with-arduino-42-railway-crossing-multi-track-two-way/
  */
+//Include the software serial library 
 #include "SoftwareSerial.h"
+
+//Include the Dfrobot player library you will neded to download it into your Arduino library location to use it.
 #include "DFRobotDFPlayerMini.h"
 
+//Include the servo h library which makes controlling the servo much easier
+#include <Servo.h>
+
+//setup the software serial port for controlling the mp3 player on pins 10 and 11
 SoftwareSerial mySoftwareSerial(10,11);
 DFRobotDFPlayerMini myDFPlayer;
 
+//setup the speed for the servos to move
 #define GATE_SPEED          35         // [ms] lower number is higher servo speed
+
+//setup the speed for the leds to blink
 #define BLINK_SPEED         400        // [ms] smaller number is faster blinking
+
+//setup 2 time delays for the leds before movements
 #define GATE_DELAY_CLOSE    2000       // [ms] time between start blinking and gate closing
 #define GATE_DELAY_OPEN     4500       // [ms] time between start blinking and gate opening
+
+//setup the 2 servos used required angles 0 deg and 100 deg and give meaninful variable names
 #define GATE_OPEN_ANGLE       0         // servo position for open 0 deg
 #define GATE_CLOSED_ANGLE    100        // servo position for closed 95  deg
+
+//setup the 2 servos used in the sketch on pins 4 and 5 and give meaninful variable names
 #define SERVO_PIN            4         // right gate servo
 #define SERVO_PIN1           5         // left gate servo
+
+//setup the 4 leds used in the sketch on pins 6,7,12,13 and give meaninful variable names
 #define LED1_PIN             6         // left bank led's
 #define LED2_PIN             7         // right bank led's
 #define LED3_PIN             12        // left bank led's
 #define LED4_PIN             13        // right bank led's
+
+//setup the 1 trigger pin used in the sketch on pin 8 and give meaninful variable name
 #define SENSOR1_PIN          8         // MERG current sensor
 
 byte led1, led2, blink_enabled;        // variables led1 = on,  led2 = off, blink_enabled 0 = off 1 = on
@@ -37,7 +57,6 @@ unsigned long time_for_servo_step;     // variable to hold the millis time to co
 
 byte sound_bell;
 
-#include <Servo.h>
 Servo gate_servo;                     // declaes the right gate servo for servo.h
 Servo gate_servo1;                    // declaes the left gate servo for servo.h
 
